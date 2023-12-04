cd9527e03136a77ef9d21f8e40bf31a08ec0a381
woramat@adsystemasia.com <qwerty.10>
2023-12-04 09:20:14 +0700

    add daisyui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
     <main className='container mx-auto pt-16'>
       <h1 className='text-4xl font-bold'>Welcome to Treasure Words</h1>
       <p className='mt-4 text-lg'>Explore the world of treasures through words!</p>
-      <button onClick={() => testapi()}>test api</button>
+      <button className="btn" onClick={() => testapi()}>Button to test API</button>
     </main>
   )
 }
