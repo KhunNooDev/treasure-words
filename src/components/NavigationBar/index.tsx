7019cf5d670748d50bc1c1c2990cae2692ae6616
KhunNooDev <noo_okok@hotmail.com>
2023-12-17 22:55:08 +0700

    update nav

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -7,6 +7,7 @@ import {
   AiOutlineUser,
   AiOutlineBell,
   AiOutlineArrowLeft,
+  AiOutlineFontColors,
 } from 'react-icons/ai'
 import { useRouter, usePathname } from 'next/navigation'
 
@@ -17,7 +18,7 @@ interface INavigationBar {
 const webTitle = 'Treasure Words'
 const navigationItems = [
   { name: 'Home', icon: AiOutlineHome, path: '/' },
-  { name: 'Words', icon: AiOutlineRocket, path: '/words' },
+  { name: 'Words', icon: AiOutlineFontColors, path: '/words' },
   { name: 'Learn & Play', icon: AiOutlineRocket, path: '/learnplay' },
   { name: 'Categories', icon: AiOutlineAppstore, path: '/categories' },
   { name: 'Profile', icon: AiOutlineUser, path: '/profile' },
@@ -29,8 +30,8 @@ export default function NavigationBar({ children }: INavigationBar) {
   const pageTitle = navigationItems.find((x) => x.path === currentPage)?.name
   return (
     <>
-      <div className='fixed left-0 right-0 top-0 bg-neutral shadow-md z-[99]'>
-        <div className='flex items-center justify-between px-4 py-2 md:hidden'>
+      <div className='fixed left-0 right-0 top-0 z-[99] bg-neutral shadow-md'>
+        <div className='flex h-14 items-center justify-between px-4 py-2 md:hidden'>
           {/* Back Icon to Home Page */}
           {currentPage !== '/' && (
             <button onClick={() => router.back()} className='flex items-center text-blue-500 focus:outline-none'>
@@ -46,24 +47,39 @@ export default function NavigationBar({ children }: INavigationBar) {
             <AiOutlineBell className='text-xl' />
           </button>
         </div>
-        <div className='hidden items-center justify-between px-4 py-2 md:flex'>
+        <div className='hidden h-14 items-center justify-between px-4 py-2 md:flex'>
           {/* Web Title */}
           <span className='text-xl font-semibold'>{webTitle}</span>
-
-          {/* NavigationItems only name */}
-          <></>
-
-          {/* Bell Icon for Notifications */}
-          <button className='text-gray-500 focus:outline-none'>
-            <AiOutlineBell className='text-xl' />
-          </button>
+          {/* Right menu */}
+          <div className='flex gap-3'>
+            <nav className='flex gap-3'>
+              <ul className='flex justify-around gap-3 py-2'>
+                {navigationItems.map(({ name, icon: Icon, path }) => (
+                  <li
+                    key={name}
+                    onClick={() => router.push(path)}
+                    className={`flex flex-auto cursor-pointer items-center justify-between gap-1 ${
+                      currentPage === path ? 'text-blue-500' : 'text-gray-500'
+                    }`}
+                  >
+                    <Icon className='text-2xl' />
+                    <span className='block text-xs'>{name}</span>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+            {/* Bell Icon for Notifications */}
+            <button className='text-gray-500 focus:outline-none'>
+              <AiOutlineBell className='text-xl' />
+            </button>
+          </div>
         </div>
       </div>
 
       {/* Children Content */}
       {children}
 
-      <nav className='fixed bottom-0 left-0 right-0 bg-neutral shadow-md md:hidden z-[99]'>
+      <nav className='fixed bottom-0 left-0 right-0 z-[99] h-14 bg-neutral shadow-md md:hidden'>
         <ul className='flex justify-around py-2'>
           {navigationItems.map(({ name, icon: Icon, path }) => (
             <li
