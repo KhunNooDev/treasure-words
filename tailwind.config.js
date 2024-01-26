bc2629c79eff0075a9419cc37dbd91dca145d9e7
KhunNooDev <noo_okok@hotmail.com>
2024-01-26 21:56:50 +0700

    update screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      screens: {
+        sml: '390px',
+      },
+    },
+  },
   plugins: [require("daisyui")],
   daisyui: {
     themes: [{
